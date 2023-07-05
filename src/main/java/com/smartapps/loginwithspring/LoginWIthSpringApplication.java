f0f90284e3dcd0e365474d53803e4a2094d21d7b
SmartApps <sammykagichu@gmail.com>
2023-07-05 10:14:21 +0300

    starting

---
+++ src/main/java/com/smartapps/loginwithspring/LoginWIthSpringApplication.java
@@ -0,0 +1,13 @@
+package com.smartapps.loginwithspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginWIthSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoginWIthSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/smartapps/loginwithspring/LoginWIthSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smartapps.loginwithspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginWIthSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
